60c07923b509c33620c0e75bfc35331e16e28b89
AlexeyGonichev <dallarnn@gmail.com>
2023-07-17 12:29:28 +0300

    initial

---
+++ funcs.py
@@ -0,0 +1,9 @@
+def math(x, y):
+    i = x + y
+    return i
+
+def other(t, r):
+    o = t - r
+    return o
+
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from funcs import math
+from funcs import other
+
+d = math(2, 4)
+print(d)
+
+s = other(7, 3)
+print(s)
